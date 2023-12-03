49657a7aa2cf2409f7141ae2fad93c81c4012d78
Geovana Santana <geo.maria117@gmail.com>
2023-12-03 10:37:48 -0300

    subindo esqueleto do projeto

---
+++ src/main/java/com/example/curiculos/CuriculosApplication.java
@@ -0,0 +1,13 @@
+package com.example.curiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuriculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuriculosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/curiculos/CuriculosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.curiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuriculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
