1e2e5a622e83cbf75465a1a0fcd877200d151650
Geovana Santana <102238021+geosantanaa@users.noreply.github.com>
2023-12-05 08:38:42 -0300

    Update CurriculoRepository.java

--- src/main/java/com/example/curiculos/repository/CurriculoRepository.java
+++ src/main/java/com/example/curiculos/repository/CurriculoRepository.java
@@ -1,9 +1,11 @@
 package com.example.curiculos.repository;
 
+import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.curiculos.model.Curriculo;
 
+@Repository
 public interface CurriculoRepository extends JpaRepository<Curriculo, Long>{
 	
 }
